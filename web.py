6e6557a67bfb9433f08b95abd8ab490b60a4b191
Melissa Jones <melissajones@Melissas-MacBook-Air.local>
2016-09-11 18:20:31 -0400

    First commit

--- web.py
+++ web.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request
-import weather
+import yelp_api
 import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     address = request.values.get('address')
-    forecast = None
+    term = request.values.get('term')
+    businesses = []
     if address:
-        forecast = weather.get_weather(address)
-    return render_template('index.html', forecast=forecast)
+        businesses = yelp_api.get_businesses(term, address)
+    # forecast = None
+    # if address:
+    #     forecast = weather.get_weather(address)
+    return render_template('index.html', businesses=businesses)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
